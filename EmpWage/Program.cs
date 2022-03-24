78dd5809d8abe8b30f02385101c3accaae44c410
VivekOwhal43 <vivekowhal.co@gmail.com>
2022-03-24 21:02:24 +0530

    [Vivek] Completed UseCase-2 Calculating Daily Wage

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -5,18 +5,28 @@ namespace EmpWage
     {
         static void Main(string[] args)
         {
-            int IS_PRESENT=0;
-            Random random = new Random();
-            int check = random.Next(0,2);
-            if(check == IS_PRESENT)
-            {
-                Console.WriteLine("Absent");
+            int IS_FULLTIME=1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empWages = 0;
+            int empHours = 0;
 
+
+            Random random = new Random();
+            
+            int check = random.Next(0,2);
+            if(check == IS_FULLTIME)
+            {
+                Console.WriteLine("Present");
+                empHours=8;
+                Console.WriteLine($"Emp {empHours} Hrs present");
             }
             else
             {
-                Console.WriteLine("Present");
+                Console.WriteLine("Absent");
+                empWages =0;
             }
+            empWages = empHours*EMP_RATE_PER_HOUR;
+            Console.WriteLine($"Employee Wages are {empWages} ");
         }
     }
 }
