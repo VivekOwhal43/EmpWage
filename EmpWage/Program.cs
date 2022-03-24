b11bdc243cbf7e6898a7e45c5c3088e0a9365106
VivekOwhal43 <vivekowhal.co@gmail.com>
2022-03-24 23:17:44 +0530

    [Vivek] Completed Use Case 5

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
 namespace EmpWage
 {
     class Program
@@ -6,55 +8,46 @@ namespace EmpWage
         public const int IS_FULLTIME = 1;
         public const int IS_PARTIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        int empWages = 0;
-        int empHours = 0;
+        public const int MONTH_DAYS = 20;
+        //int empWages = 0;
+        //int empHours = 0;
         static void Main(string[] args)
         {
             int empWages = 0;
             int empHours = 0;
+            int earnedWages = 0;
 
 
             Random random = new Random();
 
-            int check = random.Next(0, 3);
-            /*if(check == IS_FULLTIME)
+            for (int DAY = 1; DAY <= MONTH_DAYS; DAY++)
             {
-                Console.WriteLine("Present");
-                empHours=8;
-                Console.WriteLine($"Emp {empHours} Hrs present");
+                int check = random.Next(0, 3);
+                switch (check)
+                {
+                    case IS_FULLTIME:
+                        Console.WriteLine($"\nDay {DAY} \nPresent");
+                        empHours = 8;
+                        Console.WriteLine($"Emp {empHours} Hrs present");
+                        break;
+
+                    case IS_PARTIME:
+                        Console.WriteLine($"\nDay {DAY} \nPresent Part Time");
+                        empHours = 4;
+                        Console.WriteLine($"Emp {empHours} Hrs present");
+                        break;
+
+                    default:
+                        Console.WriteLine($"\nDay {DAY} \nAbsent");
+                        empWages = 0;
+                        break;
+                }
+
+
+                empWages = empHours * EMP_RATE_PER_HOUR;
+                earnedWages = empWages * DAY;
+                Console.WriteLine($"Employee Wages are {empWages}\nEarned Wages Till Date: {earnedWages}");
             }
-            else if(check == IS_PARTIME)
-            {
-                Console.WriteLine("Present Part Time");
-                empHours=4;
-                Console.WriteLine($"Emp {empHours} Hrs present");
-            }
-            else
-            {
-                Console.WriteLine("Absent");
-                empWages =0;
-            }*/
-            switch (check)
-            {
-                case IS_FULLTIME:
-                    Console.WriteLine("Present");
-                    empHours=8;
-                    Console.WriteLine($"Emp {empHours} Hrs present");
-                break;
-                
-                case IS_PARTIME:
-                     Console.WriteLine("Present Part Time");
-                     empHours=4;
-                     Console.WriteLine($"Emp {empHours} Hrs present");
-                 break;
-                
-                default:
-                     Console.WriteLine("Absent");
-                     empWages =0;
-                break;
-            }
-            empWages = empHours * EMP_RATE_PER_HOUR;
-            Console.WriteLine($"Employee Wages are {empWages} ");
         }
     }
 }
