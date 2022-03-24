9e4675fde1f383c2629eac524cf28b862b678075
VivekOwhal43 <vivekowhal.co@gmail.com>
2022-03-24 22:17:59 +0530

    [Vivek] Completed Use Case-3

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -6,6 +6,7 @@ namespace EmpWage
         static void Main(string[] args)
         {
             int IS_FULLTIME=1;
+            int IS_PARTIME=2;
             int EMP_RATE_PER_HOUR = 20;
             int empWages = 0;
             int empHours = 0;
@@ -13,11 +14,17 @@ namespace EmpWage
 
             Random random = new Random();
             
-            int check = random.Next(0,2);
+            int check = random.Next(0,3);
             if(check == IS_FULLTIME)
             {
                 Console.WriteLine("Present");
                 empHours=8;
+                Console.WriteLine($"Emp {empHours} Hrs present");
+            }
+            else if(check == IS_PARTIME)
+            {
+                Console.WriteLine("Present Part Time");
+                empHours=4;
                 Console.WriteLine($"Emp {empHours} Hrs present");
             }
             else
