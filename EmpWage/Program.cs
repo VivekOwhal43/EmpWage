5640d11deb7aab236f9ba865f13c84ec784c86b7
VivekOwhal43 <vivekowhal.co@gmail.com>
2022-03-24 22:40:37 +0530

    [Vivek] Completed Use Case 4

--- EmpWage/Program.cs
+++ EmpWage/Program.cs
@@ -3,19 +3,21 @@ namespace EmpWage
 {
     class Program
     {
+        public const int IS_FULLTIME = 1;
+        public const int IS_PARTIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        int empWages = 0;
+        int empHours = 0;
         static void Main(string[] args)
         {
-            int IS_FULLTIME=1;
-            int IS_PARTIME=2;
-            int EMP_RATE_PER_HOUR = 20;
             int empWages = 0;
             int empHours = 0;
 
 
             Random random = new Random();
-            
-            int check = random.Next(0,3);
-            if(check == IS_FULLTIME)
+
+            int check = random.Next(0, 3);
+            /*if(check == IS_FULLTIME)
             {
                 Console.WriteLine("Present");
                 empHours=8;
@@ -31,8 +33,27 @@ namespace EmpWage
             {
                 Console.WriteLine("Absent");
                 empWages =0;
+            }*/
+            switch (check)
+            {
+                case IS_FULLTIME:
+                    Console.WriteLine("Present");
+                    empHours=8;
+                    Console.WriteLine($"Emp {empHours} Hrs present");
+                break;
+                
+                case IS_PARTIME:
+                     Console.WriteLine("Present Part Time");
+                     empHours=4;
+                     Console.WriteLine($"Emp {empHours} Hrs present");
+                 break;
+                
+                default:
+                     Console.WriteLine("Absent");
+                     empWages =0;
+                break;
             }
-            empWages = empHours*EMP_RATE_PER_HOUR;
+            empWages = empHours * EMP_RATE_PER_HOUR;
             Console.WriteLine($"Employee Wages are {empWages} ");
         }
     }
